6424d76e536af625be3a006bf1e0f9ab3d4fc7b7
Gabriel Forny <gfmtech21@gmail.com>
2026-04-08 18:02:41 -0300

    Subindo no mac

--- mobile/lib/core/supabase/supabase_config.dart
+++ mobile/lib/core/supabase/supabase_config.dart
@@ -1,5 +1,5 @@
 class SupabaseConfig {
-  static const String url = 'https://eavtlfqntxclvunzlwpj.supabase.co';
+  static const String url = 'https://fsetimvdfxonzqvexhst.supabase.co';
   static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdnRsZnFudHhjbHZ1bnpsd3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDc0MjksImV4cCI6MjA4NTM4MzQyOX0.FnoNUJtJjtAO2LAdgXOnCh_wRy-YiDlvOvXVlx-ouRg';
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzZXRpbXZkZnhvbnpxdmV4aHN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODk0MTEsImV4cCI6MjA5MTE2NTQxMX0.r7LsdzvYKdK0B8t-ADc5aPhU2_OolI_62UXQEJNmkFA';
 }
